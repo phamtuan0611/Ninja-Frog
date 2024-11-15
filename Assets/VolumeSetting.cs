ab0ef33f877d99337bfb834ec95745a60a60da6e
phamtuan0611 <phamtuan06112003@gmail.com>
2024-11-15 23:51:36 +0700

    create PopupSetting

---
+++ Assets/VolumeSetting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSetting : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+    }
+}
